f0e09f5800d4688bcbcdaf7a61c172b8ebe18370
Debasree Samadder <39428701+debasree888@users.noreply.github.com>
2021-08-31 00:28:28 +0530

    Update DisplayArrayValues.java
    added program description in comments

--- myArrays/DisplayArrayValues.java
+++ myArrays/DisplayArrayValues.java
@@ -1,6 +1,27 @@
-//Write a java program to create an array of size ‘n’
-// and accepts ‘n’ number of integers from the user
-// and display all the values from an array
+/*Write a java program to create an array of size ‘n’ and accepts ‘n’ number of integers from the user and display all the values from an array
+
+Sample Input 1
+
+Enter the array size
+
+3
+
+Enter the values
+
+46
+
+83
+
+67
+
+Sample Output 1
+
+67
+
+83
+
+46
+*/
 
 package myArrays;
 
